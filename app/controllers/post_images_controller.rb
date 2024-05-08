44cb925ce8f316a955df133bc742817cdf52775b
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-05-08 15:09:38 +0000

    [Finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,28 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+    # @post_image には form_with に渡すための「空のモデル」を用意したい
+  end
+  
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+    # リダイレクト先はinde投稿一覧
   end
 
   def index
   end
 
   def show
+  end
+  
+   # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
