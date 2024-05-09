67433d7d20c24241f1775fba72024687c7b38038
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-05-09 11:34:32 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # 画像投稿post_imagesには、「新規投稿」「一覧」「詳細機能」「削除」のみ
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
