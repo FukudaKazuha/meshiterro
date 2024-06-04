c23a6aa4f59934b0882702a247989dcf555ff875
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-06-04 10:53:40 +0000

    [Add]dashboards機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
   # get 'users/show'
   # get 'users/edit'
   resources :users, only: [:show, :edit, :update]
